18108e7503bbf834779c900c071bc2ff3123ebe8
jungbee2 <jungb1203@naver.com>
2023-02-28 09:56:46 +0900

    local to git

--- jspwork/JspAjaxProject/src/main/java/memo/model/MemoDao.java
+++ jspwork/JspAjaxProject/src/main/java/memo/model/MemoDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -80,5 +81,95 @@ public class MemoDao{
 		
 		return list;
 	}
-}
+	
+	//삭제
+	public void deleteMemo(String num)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt =null;
+		
+		String sql="delete from memo where num=?";
+	
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		
+	}
+	
+	//num에 해당하는 하나의 데이터 리턴
+	public MemoDto getData(String num)
+	{
+		MemoDto dto= new MemoDto();
+		
+		Connection conn= db.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs= null;
+		
+		String sql="select * from memo where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, num);
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next())
+			{
 
+				dto.setNum(rs.getString("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setContent(rs.getString("content"));
+				dto.setAvata(rs.getString("avata"));
+				dto.setWriteday(rs.getTimestamp("writeday"));
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		return dto;
+	}
+	
+
+	//수정
+	  
+	   public void updateMemo(MemoDto dto) {
+		   
+		   Connection conn=db.getConnection();
+		   PreparedStatement pstmt=null;
+		   
+		   String sql="update memo set writer=?,content=?,avata=? where num=?";
+		   
+		   try {
+			   
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getContent());
+			pstmt.setString(3, dto.getAvata());
+			pstmt.setString(4, dto.getNum());
+			
+			pstmt.execute();
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		   
+	   }
+	   
+	}
